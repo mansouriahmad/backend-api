5ad769ed763673dc0f75785f10271740b87db101
Seyed Ahmad Mansouri <amansouri@amctechnology.com>
2022-12-10 12:14:49 -0500

    updated main and run to use a random port

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,10 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let port = listener.local_addr().unwrap().port();
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
         .run();
+    println!("Server is running on port {} ...", port);
     Ok(server)
 }
